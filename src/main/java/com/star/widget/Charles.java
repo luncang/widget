b08d78f5394db5f7b3b3dd2df360bf7b57b8d797
charles <luncang1988@163.com>
2017-09-05 16:16:31 +0800

    first commit

---
+++ src/main/java/com/star/widget/Charles.java
@@ -0,0 +1,15 @@
+package com.star.widget;
+
+import android.util.Log;
+
+/**
+ * Created by Luncang on 2017/9/5.
+ * Description:
+ */
+
+public class Charles {
+
+    public static void test(){
+        Log.e("mess","test..........");
+    }
+}
